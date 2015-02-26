01e3366167e8b5830f3136834355ff193172b1c6
franknelson <fnelson12@gmail.com>
2015-02-25 20:42:41 -0600

    initial commit
    building a Wordpress site for Sanctuary

---
+++ content-single.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @package _sanctuary
+ */
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<header class="entry-header">
+		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+
+		<div class="entry-meta">
+			<?php _sanctuary_posted_on(); ?>
+		</div><!-- .entry-meta -->
+	</header><!-- .entry-header -->
+
+	<div class="entry-content">
+		<?php the_content(); ?>
+		<?php
+			wp_link_pages( array(
+				'before' => '<div class="page-links">' . __( 'Pages:', '_sanctuary' ),
+				'after'  => '</div>',
+			) );
+		?>
+	</div><!-- .entry-content -->
+
+	<footer class="entry-footer">
+		<?php _sanctuary_entry_footer(); ?>
+	</footer><!-- .entry-footer -->
+</article><!-- #post-## -->
