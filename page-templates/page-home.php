edcb17bb2a889a39f0edda400bdf0e0162bac8da
franknelson <fnelson12@gmail.com>
2015-03-11 23:21:27 -0500

    added advanced custom fields to the home page and styled the text

--- page-templates/page-home.php
+++ page-templates/page-home.php
@@ -8,7 +8,47 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
+        
             
+            <div class="bkd group1">
+            
+                <img src="<?php the_field('bkd_img_1'); ?>" alt="" />
+                <p class="line1"><?php the_field('bkd_1_text'); ?></p>
+                <p class="line2"><?php the_field('bkd_1_text_2'); ?></p>
+            
+            </div>
+            
+            <div class="bkd group2">
+            
+                <img src="<?php the_field('bkd_img_2'); ?>" alt="" />
+                <p class="line1"><?php the_field('bkd_2_text'); ?></p>
+                <p class="line2"><?php the_field('bkd_2_text_2'); ?></p>
+            
+            </div>
+            
+            <div class="bkd group3">
+            
+                <img src="<?php the_field('bkd_img_3'); ?>" alt="" />
+                <p class="line1"><?php the_field('bkd_3_text'); ?></p>
+                <p class="line2"><?php the_field('bkd_3_text_2'); ?></p>
+            
+            </div>
+            
+            <div class="bkd group4">
+            
+                <img src="<?php the_field('bkd_img_4'); ?>" alt="" />
+                <p class="line1"><?php the_field('bkd_4_text'); ?></p>
+                <p class="line2"><?php the_field('bkd_4_text_2'); ?></p>
+            
+            </div>
+            
+            <div class="bkd group5">
+            
+                <img src="<?php the_field('bkd_img_5'); ?>" alt="" />
+                <p class="line1"><?php the_field('bkd_5_text'); ?></p>
+                <p class="line2"><?php the_field('bkd_5_text_2'); ?></p>
+            
+            </div>
             
 		</main>
 	</div>
